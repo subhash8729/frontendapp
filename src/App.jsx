c859d0569bb689f6132a012b0d0a4c5dacc01067
Subhash Dhaka <subhash8729@gmail.com>
2025-08-04 12:11:39 +0530

    login error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './pages/login'
 import Signup from './pages/Signup'
 import About from './pages/About'
 import Chat from './pages/chat'
 import Practice from './pages/Practice'
+import Login from './pages/Login'
 
 
 const App = () => {
